e7e0049fbca614fb9722af07514540b03b72b630
Heidi Kasemir <heidi.kasemir@gmail.com>
2018-01-01 12:51:02 -0800

    add scrolling to list view

--- components/HomeView.js
+++ components/HomeView.js
@@ -2,7 +2,6 @@ import styled from 'styled-components/native';
 
 export default styled.View`
   flex: 1;
-  padding-top: 15;
   background-color: #eee;
   align-items: center;
   justify-content: flex-start;
--- views/DeckList.js
+++ views/DeckList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {View, Text, TextInput, KeyboardAvoidingView} from 'react-native';
+import {FlatList} from 'react-native';
 import HomeView from '../components/HomeView';
 import DeckListItem from '../components/DeckListItem.js';
 import storageHelper from '../helpers/storage-helper';
@@ -22,16 +22,21 @@ export default class DeckList extends React.Component {
     });
   }
 
+  renderDeckListItem = ({item}) => {
+    const {decks, navigation} = this.props;
+    return <DeckListItem key={item} deck={decks[item]} onPress={(deck) => navigation.navigate('DeckView', {deck})}/>
+  }
+
   render() {
-    const {navigation, decks} = this.props;
+    const {decks} = this.props;
     const deckNames = Object.keys(decks);
     return (
       <HomeView>
-        {
-          deckNames.map(title => (
-            <DeckListItem key={title} deck={decks[title]} onPress={(deck) => navigation.navigate('DeckView', {deck})}/>
-          ))
-        }
+        <FlatList
+          style={{alignSelf: 'stretch'}}
+          data={deckNames}
+          renderItem={this.renderDeckListItem}
+        />
       </HomeView>
     );
   }
